028421b6a84d3d8e8d93f4ee33291c358d6b7a3d
Christoph Englert <christoph.englert@axtesys.at>
2023-07-08 15:53:46 +0200

    add timer and start button

---
+++ Assets/Scripts/RunTimer.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEditor;
+using UnityEngine;
+
+public class RunTimer : MonoBehaviour
+{
+    [SerializeField] private TMP_Text timeText;
+    [SerializeField] private ThiefAI thiefAi;
+
+    private float currentTime;
+
+    private void OnEnable()
+    {
+        thiefAi.OnRunStarted += StartTimer;
+        thiefAi.OnRunFinished += EndTimer;
+    }
+
+    private void OnDisable()
+    {
+        thiefAi.OnRunStarted -= StartTimer;
+        thiefAi.OnRunFinished -= EndTimer;
+    }
+
+    private void StartTimer()
+    {
+        currentTime = 0;
+        Debug.Log("Timer S");
+        StartCoroutine(nameof(Timer_Courtine));
+    }
+
+    private void EndTimer()
+    {
+        StopCoroutine(nameof(Timer_Courtine));
+        SetTimeField();
+    }
+
+    private IEnumerator Timer_Courtine()
+    {
+        while (true)
+        {
+            currentTime += Time.deltaTime;
+            SetTimeField();
+            yield return null;
+        }
+    }
+
+    private void SetTimeField()
+    {
+        timeText.text = currentTime.ToString();
+    }
+}
