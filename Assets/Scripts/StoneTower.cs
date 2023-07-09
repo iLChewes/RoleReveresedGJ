404a8aad94773a84007db782e08bb9d93badf53c
Christoph Englert <christoph.englert@axtesys.at>
2023-07-09 17:36:38 +0200

    add Stone Tower

---
+++ Assets/Scripts/StoneTower.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Burst.CompilerServices;
+using UnityEngine;
+
+public class StoneTower : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem shockWaveVfx;
+
+    private bool isActive = true;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!isActive) return;
+        if (collision.TryGetComponent<ThiefAI>(out ThiefAI thiefAi))
+        {
+            isActive = false;
+            shockWaveVfx.Play();
+            Vector2 dir =  thiefAi.transform.position - transform.position;
+            thiefAi.Rückstoß(dir);
+        }
+    }
+
+
+
+}
